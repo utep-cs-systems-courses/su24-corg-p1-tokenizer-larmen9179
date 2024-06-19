fff8983ad8c2016c633b1d06c1617c6f440ce72d
logan <lo.j.armendariz@gmail.com>
2024-06-18 18:39:33 -0600

    completed gui

--- src/main.c
+++ src/main.c
@@ -2,9 +2,21 @@
 #include "tokenizer.h"
 #include "history.h"
 
+#define MAXLEN 100
+
 int main(int argc, char **argv){
 
-  printf("Test for running Makefile command\n");
+  int c;
 
-  return 0;
+  putchar('$');
+  
+  while((c = getchar()) != EOF){
+    if(c == '\n'){
+      putchar(c);
+      putchar('$');
+    }
+    else{
+      putchar(c);
+    }
+  }
 }
